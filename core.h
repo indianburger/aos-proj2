017e4fa61a5a86d662c5f2406ccc755dec1fad42
=Aravind Ramanathan <aravindr85@gmail.com>
2011-03-02 02:30:36 -0500

    modified core.h and make file

--- core.h
+++ core.h
@@ -6,57 +6,100 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define REQ_Q_KEY "req_key_file" 
-#define RESP_Q_KEY "resp_key_file"
-#define REQ_SEM_KEY "sem_key_file"
-
+#define REQ_KEY_FILE "req_key_file" 
+#define RESP_KEY_FILE "resp_key_file" 
+#define SEM_KEY_FILE "sem_key_file" 
+#define REQ_Q 0
+#define RESP_Q 1
+#define KEY_ID 1
 typedef struct diffie_pkt {
     long mtype;
+    int req_id;
     int inp_x;
     int inp_p;
     int out_result;
 } diffie_pkt;
 
-int get_req_q_id(){
+
+int put_pkt(int key_id, diffie_pkt pkt);
+int get_q_id(int key_id);
+int get_q_id_gen(int key_id, int flag);
+int get_key_id(int key_id);
+diffie_pkt get_pkt(int key_id);
+void initialize();
+void clean_up();
+/* QUEUE FUNCTIONS */
+int put_pkt(int key_id, diffie_pkt pkt){
+    int msqid = get_q_id(key_id); 
+
+    if (msgsnd(msqid, &pkt, sizeof(diffie_pkt), IPC_NOWAIT) < 0) {
+        fprintf(stderr, "\nput_pkt error for key_id: %d", key_id);
+        return -1; 
+    }
+    return 0;
+}
+
+int get_q_id(int key_id){
+    int x = get_q_id_gen(key_id, -1);
+    fprintf(stderr, "\nget_q_id: key id %d for %d", x, key_id);
+    return x;
+}
+
+int get_q_id_gen(int key_id, int flag){
     int msqid;
-    int msgflg = IPC_CREAT | 0666;
-    key_t key = ftok(REQ_Q_KEY, 'a');
+    int msgflg;
+    if (flag == -1) msgflg = 0666;
+    else msgflg = flag | 0666;
+    key_t key = get_key_id(key_id);
     
     if ((msqid = msgget(key, msgflg )) < 0) {
-        perror("msgget");
-        return -1;
+        fprintf(stderr, "\nmsgget error for %d", key_id);
     }
-    else 
-        fprintf(stderr,"msgget: msgget succeeded: msqid = %d\n", msqid);
     return msqid;
 }
 
-int send_request(int pid, int x, int p){
-    int msqid = get_req_q_id(); 
-    diffie_pkt pkt;
-
-    pkt.mtype = 1; 
-    pkt.inp_x = x;
-    pkt.inp_p = p;
-
-    if (msgsnd(msqid, &pkt, sizeof(diffie_pkt), IPC_NOWAIT) < 0) {
-        printf ("msqid: %d, type: %ld, x: %d, p: %d\n", msqid, pkt.mtype, pkt.inp_x, pkt.inp_p);
-        perror("msgsnd");
-        exit(1);
-    }
-    
-    return 0;
+int get_key_id(int key_id){
+    int x = ftok(key_id == REQ_Q ? REQ_KEY_FILE: RESP_KEY_FILE, 1);
+    fprintf(stderr, "\nget_key_id: key id %d for %d", x, key_id);
+    return x;
 }
-      
-int rcv_response(){
-    int msqid;
-    int msqid = get_req_q_id(); 
+
+diffie_pkt get_pkt(int key_id){
+    int msqid = get_q_id(key_id); 
     diffie_pkt pkt;
-    if (msgrcv(msqid, &pkt, sizeof(diffie_pkt), 1, 0) < 0) {
-        perror("msgrcv");
-        exit(1);
+    if (msgrcv(msqid, &pkt, sizeof(diffie_pkt), 0, 0) < 0) {
+        fprintf(stderr, "\nmsgget error for %d", key_id);
     }
-    printf("Received this: %ld %d %d\n", pkt.mtype, pkt.inp_p, pkt.inp_x);
-    return 1;
+    else
+        pkt.out_result = pkt.inp_p * pkt.inp_x;
+    //printf("Received this: %ld %d %d\n", pkt.mtype, pkt.inp_p, pkt.inp_x);
+    //put_pkt(RESP_KEY_ID, pkt);
+    return pkt;
+}
+
+/* SEMAPHORE FUNCTIONS */
+
+/* GLOBAL INITIALIZE AND CLEANUP */
+void initialize(){
+    get_q_id_gen(REQ_Q, IPC_CREAT);
+    get_q_id_gen(RESP_Q, IPC_CREAT);
+}
+
+
+void clean_up(){
+    int msqid;
+
+
+    fprintf(stderr, "\nclean_up: req");
+    msqid = get_q_id(REQ_Q);
+
+    if (msgctl(msqid, IPC_RMID, NULL) < 0)
+        fprintf(stderr, "\n\nError clearing queue %d and msqid %d", REQ_Q, msqid);
+    
+
+    fprintf(stderr, "\nclean_up: resp");
+    msqid = get_q_id(RESP_Q);
+    if (msgctl(msqid, IPC_RMID, NULL) < 0)
+        fprintf(stderr, "\n\nError clearing queue %d and msqid %d", RESP_Q, msqid);
 }
 
--- makefile
+++ makefile
@@ -1,5 +1,6 @@
-edit: client.c include.h 
+compile: client.c core.h 
 	gcc -o client client.c
-	touch req_key_file resp_key_file sem_key_file
+	touch req_key_file resp_key_file sem_key_file 
+	
 clean: 
-	rm client req_key_file resp_key_file sem_key_file
+	rm client req_key_file resp_key_file sem_key_file 
