ee99a24df5d08c9b8c9b5a51e0541a6a2b78a2f1
=Aravind Ramanathan <aravindr85@gmail.com>
2011-03-02 04:09:31 -0500

    adding sem changes in core. and rubbish in client.c

--- client.c
+++ client.c
@@ -5,13 +5,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "include.h"
+#include "core.h"
 int send_request(int pid, int x, int p);  
 int rcv_response();
 int main ( int argc, char *argv[] )
 {
-    send_request(getpid(), 1, 2);
-    rcv_response();
+    //send_request(getpid(), 1, 2);
+    //rcv_response();
     return EXIT_SUCCESS;
 }
 
