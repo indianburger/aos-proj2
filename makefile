de24d5d81e72b8ffa52d81aa99f3070485c3ec7e
nadu <narayanan.ramakrishnan@gmail.com>
2011-03-03 18:08:37 -0500

    Added function generatePrimes for generating huge values of p and results are now printed with corresponding request ids

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 compile: client.c core.h realclient.c server.c
 	gcc -o client client.c
 	gcc -o server server.c
-	gcc -o realclient realclient.c
+	gcc -o realclient realclient.c -lcrypto -lm
 
 	touch req_key_file resp_key_file sem_key_file 
 	
--- realclient.c
+++ realclient.c
@@ -5,9 +5,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <openssl/bn.h>
+#include <math.h>
 #include "core.h"
 
 void initPacket(int i, int pid, diffie_pkt *pkt);
+long long int generatePrime(int d);
 
 int main(int argc, char *argv[])
 {
@@ -23,7 +26,7 @@ int main(int argc, char *argv[])
 		for(i=0;i<num_of_requests;i++)
 		{
 			diffie_pkt p ;
-			fprintf(stderr,"process with pid =  %d , number of requests = %d, and this is request # = %d\n", getpid(), num_of_requests, i);
+			fprintf(stderr,"\n Sending packet for process with pid =  %d , number of requests = %d, and this is request # = %d\n", getpid(), num_of_requests, i);
 			// create packet 
 			//diffie_pkt p = (diffie_pkt*)malloc(sizeof(diffie_pkt));
 			initPacket(i, getpid(), &p);
@@ -36,7 +39,7 @@ int main(int argc, char *argv[])
 			}
 			// now that i have put the packet in the queue i want to use it
 			diffie_pkt result =  get_pkt(RESP_Q, getpid());
-			fprintf(stderr," This is the sync result received from the server %lld\n", result.out_result);
+			fprintf(stderr,"\n Sync result received from the server for process with pid = %d request #= %d  is %lld\n", getpid(), result.req_id, result.out_result);
 			
 		}
 	}
@@ -49,7 +52,7 @@ int main(int argc, char *argv[])
 		for(i=0;i<num_of_requests;i++)
 		{
 			diffie_pkt p ;
-			fprintf(stderr, "\nprocess with pid =  %d , number of requests = %d, and this is request # = %d\n", getpid(), num_of_requests, i);
+			fprintf(stderr, "\n Sending packet for process with pid =  %d , number of requests = %d, and this is request # = %d\n", getpid(), num_of_requests, i);
 			
 			// create packet 
 			//diffie_pkt p = (diffie_pkt*)malloc(sizeof(diffie_pkt));
@@ -65,12 +68,12 @@ int main(int argc, char *argv[])
 			if(i%10 == 0){
 				// now that i have put the packet in the queue i want to use it
 				do{
-					fprintf(stderr,"\nrealclient: inside 10");
+					//fprintf(stderr,"\nrealclient: inside 10");
 					result = get_pkt_async(RESP_Q, getpid());
 					
 					if(result.mtype != -1){
 						async_pkt_ctr--;
-						fprintf(stderr,"\nNOT DRAINED realclient:result received at realclient pid =%d from the server %lld\n", getpid(), result.out_result);
+						fprintf(stderr,"\n Async result received from the server for process with pid = %d request #= %d  is %lld\n", getpid(), result.req_id, result.out_result);
 					}
 					
 				}while(result.mtype != -1);
@@ -88,7 +91,7 @@ int main(int argc, char *argv[])
 					result = get_pkt_async(RESP_Q, getpid());
 					if(result.mtype != -1){
 						async_pkt_ctr--;
-						fprintf(stderr,"\nDRAINING: result received at realclient pid =%d from the server %lld\n", getpid(), result.out_result);
+						fprintf(stderr,"\nDRAINING: result received for pid =%d request # = %d from the server is %lld\n", getpid(), result.req_id, result.out_result);
 					}
 					
 				}while(async_pkt_ctr != 0);
@@ -103,10 +106,42 @@ void initPacket(int i, int pid, diffie_pkt *pkt){
 	// give it some values
 	
 	pkt->inp_x = i*3 % 63;
-	pkt->inp_p = DUMMY_PVALUE;
+	pkt->inp_p = generatePrime(50);//DUMMY_PVALUE;
 	pkt->mtype = pid;
 	pkt->req_id = i;	
 	
-//	return pkt;
+//	return pkt;	
+}
+
+void success(){}
+long long int generatePrime(int dummy){
+	
+	char *prime;
+	BIGNUM *num_tmp;
+	long int num_bits = 50; // 50 bit large prime number will be generated
+	int i;
+	int base=10;
+	long long int num = 0;
+	int len;
+	
+	if(dummy==0)
+		return DUMMY_PVALUE;	
+	
+	num_tmp = BN_new();
+	BN_generate_prime(num_tmp, num_bits,1,NULL,NULL,success,NULL);
+	prime = (char*)malloc(BN_num_bytes(num_tmp));
+	prime = BN_bn2dec(num_tmp);
+		
+	len = strlen(prime)-1;
+	
+	for(i = 0; i < strlen(prime); i++){
+		//printf("num = %lld\n", num);
+		num = num+ pow(base,(len-i))*(prime[i]-'0');
+	}
+	//printf("in generate prime - after conversion prime generated  = %lld \n",num );
+	return num;
+	
 	
 }
+
+
